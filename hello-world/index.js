fbeacb42ffdb992d7b238d37c12b602376849d2b
Madhav Varanasi <62279010+madhavvaranasi3@users.noreply.github.com>
2023-03-18 11:20:17 +0530

    Initial commit

---
+++ hello-world/index.js
@@ -0,0 +1,8 @@
+const hello = () => {
+  // eslint-disable-next-line quotes, semi
+  console.log("Hello GitHub!");
+  // eslint-disable-next-line semi
+};
+
+// eslint-disable-next-line semi
+hello();
